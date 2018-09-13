c6223956feab9631383f76202eb56ce863ef94ce
Quique Fdez Guerra <quique@ckgrafico.com>
2018-09-13 16:40:47 +0200

    Fix spy

--- src/app/cities/city-list/city-list.component.spec.ts
+++ src/app/cities/city-list/city-list.component.spec.ts
@@ -1,6 +1,6 @@
 import 'test/unit/karma.vendor';
-import { shallowMount, createLocalVue } from '@vue/test-utils'
-import sinon from 'sinon'
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import sinon from 'sinon';
 import { expect } from 'chai';
 
 import { mockInject } from '~/core';
@@ -10,7 +10,7 @@ import { default as CityListComponent } from './city-list.component.vue';
 // Simple Stub test
 
 describe('CityListComponent', () => {
-  let localVue
+  let localVue;
 
     beforeEach(() => {
       localVue = createLocalVue();
@@ -22,16 +22,15 @@ describe('CityListComponent', () => {
                 localVue
             });
 
-            const spyCitiesServiceRemove = sinon.stub()
+            const spyCitiesServiceRemove = sinon.stub();
             mockInject(wrapper.vm, 'citiesService', {
                 remove: spyCitiesServiceRemove
-            })
+            });
         
-            await nextTick()
+            await nextTick();
             wrapper.vm.remove();
         
             expect(spyCitiesServiceRemove).to.be.called;
-            spyCitiesServiceRemove.restore();
         });
     });
 });
